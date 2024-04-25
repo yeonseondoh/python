2f4b6582e47e6aa5ce9f0fc9cb6617e882535a3a
yeonseondoh <112925963+yeonseondoh@users.noreply.github.com>
2024-04-25 20:38:54 +0900

    hw6.py

---
+++ hw6.py
@@ -0,0 +1,16 @@
+for i in range(1, 10):
+    for j in range(2, 6):  
+        result = i * j
+        print(j, 'x', i, '=', result, end = '\t')
+        if j == 5:  
+            print()
+
+  
+print('')
+
+for i in range(1, 10):
+    for j in range(6, 10):  
+        result = i * j
+        print(j, 'x', i, '=', result, end = '\t')
+        if j == 9:  
+            print()
